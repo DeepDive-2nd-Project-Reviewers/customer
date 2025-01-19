efe88f91c63f2eb7eb4d1b239c820b29cb2af096
hyunoi <nhe0622@gmail.com>
2025-01-20 02:52:02 +0900

    feat: AssignmentGradeController 생성

--- src/main/java/reviewers/customer/domain/assignmentGrade/controller/AssignmentController.java
+++ src/main/java/reviewers/customer/domain/assignmentGrade/controller/AssignmentController.java
@@ -1,4 +1,22 @@
 package reviewers.customer.domain.assignmentGrade.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reviewers.customer.domain.assignmentGrade.dto.AssignmentGradeListResponseDto;
+import reviewers.customer.domain.assignmentGrade.dto.AssignmentGradeRequestDto;
+import reviewers.customer.domain.assignmentGrade.service.AssignmentService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
 public class AssignmentController {
+    private final AssignmentService assignmentService;
+
+    @GetMapping("assignment/grades")
+    public AssignmentGradeListResponseDto getAssignmentGradeByStudentId(@RequestBody AssignmentGradeRequestDto request) {
+        return assignmentService.findAllByStudentId(request.getStudentId());
+    }
 }

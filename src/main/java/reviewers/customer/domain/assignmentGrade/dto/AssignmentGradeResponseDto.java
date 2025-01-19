eec4a79959b892edf4a386d980777ac7294ec91e
hyunoi <nhe0622@gmail.com>
2025-01-20 02:51:12 +0900

    feat: AssignmentGrade DTO 생성

---
+++ src/main/java/reviewers/customer/domain/assignmentGrade/dto/AssignmentGradeResponseDto.java
@@ -0,0 +1,16 @@
+package reviewers.customer.domain.assignmentGrade.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class AssignmentGradeResponseDto {
+    private long assignmentId;
+    private String title;
+    private long grade;
+    private String feedback;
+    private LocalDateTime submissionDate;
+}
